1a723dc355979efe5ca8d45d35d5255ea3f44c2b
tompoyeau-sii <tom.poyeau@sii.fr>
2024-05-27 14:42:57 +0200

    ajout liste de destinations

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,48 +1,53 @@
 <template>
-  <l-map :zoom="zoom" :center="center" style="height: 100vh; width: 100%;">
-    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="marker"></l-marker>
-  </l-map>
+  <div>
+    <RouteList :routes="routes" @selectRoute="showRoute"></RouteList>
+    <l-map :zoom="zoom" :center="center" style="height: 100vh; width: 100%;">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker v-if="marker" :lat-lng="marker"></l-marker>
+      <l-polyline v-if="route" :lat-lngs="route" color="red"></l-polyline>
+    </l-map>
+  </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker, LPolyline } from "@vue-leaflet/vue-leaflet";
+import RouteList from "./RouteList.vue";
 
 export default {
   components: {
     LMap,
     LTileLayer,
     LMarker,
+    LPolyline,
+    RouteList
   },
   data() {
     return {
-      zoom: 13,
-      center: [51.505, -0.09],
+      zoom: 8,
+      center: [48.0061, 0.1996], // Coordonnées du Mans par défaut
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution: '© OpenStreetMap contributors',
-      marker: [51.505, -0.09],
+      marker: null,
+      route: null,
+      routes: [
+        { from: 'Paris', to: 'Louvier', coordinates: [[48.8566, 2.3522], [49.1944, 0.3803]] },
+        { from: 'Le Mans', to: 'Laval', coordinates: [[48.0061, 0.1996], [48.0719, -0.7683]] },
+        { from: 'Nantes', to: 'Rennes', coordinates: [[47.2184, -1.5536], [48.1173, -1.6778]] }
+      ]
     };
   },
-  mounted() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.setPosition, this.handleError);
-    }
-  },
   methods: {
-    setPosition(position) {
-      const { latitude, longitude } = position.coords;
-      this.center = [latitude, longitude];
-      this.marker = [latitude, longitude];
-    },
-    handleError(error) {
-      console.error("Erreur de géolocalisation :", error);
-    },
-  },
+    showRoute(route) {
+      this.route = route.coordinates;
+      this.center = this.route[0];
+    }
+  }
 };
 </script>
 
 <style>
 .leaflet-container {
+  z-index: 1;
   height: 100vh;
   width: 100%;
 }
---
+++ src/components/RouteList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="route-list">
+    <div v-for="(route, index) in routes" :key="index" @click="selectRoute(route)">
+      {{ route.from }} - {{ route.to }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    selectRoute(route) {
+      this.$emit('selectRoute', route);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.route-list {
+  z-index: 10;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: white;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+.route-list > div {
+  cursor: pointer;
+  margin-bottom: 5px;
+}
+</style>
